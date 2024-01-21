fd907cbb561948412c4af738c97bf77f293277e2
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-01-21 03:31:40 -0500

    CSS move to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-      extend: {},
+      extend:{
+        colors:{
+          'nfl-red': '#D50A0A',
+          'nfl-blue': '#013369',
+          'nfl-white': '#FFFFFF',
+          'afc-primary': '#D50A0A',
+          'afc-secondary': '#FFFFFF',
+          'nfc-primary': '#013369',
+          'nfc-secondary': '#FFFFFF'
+        }
+      }
     },
     plugins: [],
   };
